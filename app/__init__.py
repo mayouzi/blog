a482447639e1f72559537dbac7849262c13d36ce
mayouzi <mayouzi@huizhaofang.com>
2017-11-22 22:30:06 +0800

    supervisor+uwsgi submit

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'mayor'
+
+
+from flask import Flask
+
+# 创建Flask类的实例
+app = Flask(__name__)
+
+# 从config.py读入配置
+app.config.from_object("config")
+
+# 这个import语句放在这里, 防止views, models import发生循环import
+from app import views, models
+
+
--- app/views.py
+++ app/views.py
@@ -3,3 +3,13 @@
 __author__ = 'mayor'
 
 
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    home_title = 'Welcome to mayor`s blog'
+    html_file = 'welcome.html'
+
+    return render_template(html_file, title=home_title)
